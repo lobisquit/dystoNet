85be0522a09135c0de2f52ca7b72bfd4086e543e
Enrico Lovisotto <enricolovisotto@gmail.com>
2017-03-16 11:20:25 +0100

    Doc enhancement

--- src/problem1.cpp
+++ src/problem1.cpp
@@ -4,62 +4,85 @@
 const int N = 2000; // Number of nodes
 const int K = 1000; // Number of sensing nodes
 const double deltad = 0.05; // Violation probability
+
 // Constants for Robust Soliton Distribution
 const double c = 0.01;
 const double delta = 0.05;
+const double R = c*log(K/delta)*sqrt(K);
 
 double getmu(double R, int d);
 double getro(int d);
 double gettau(double R, int d);
 
 main() {
-  double R = c*log(K/delta)*sqrt(K);
-  int d = 1; // Node-degree
-  double mu = getmu(R,d);
-  printf("%f\n",mu);
-  return 0;
-}
-
-double getro(int d){
-  double ro;
-
-  if(d > 1)
-    ro = 1./(d*(d-1));
-  else
-    ro = 1./K;
-  return ro;
-}
-
-double gettau(double R, int d){
-  double tau;
-
-  if(d>=1 && d<K/R)
-    tau = R/(d*K);
-  else if(d == K/R)
-    tau = R*log(R/delta)/K;
-  else
-    tau = 0;
-  return tau;
+	int d = 1; // Node-degree
+	double mu = getmu(R, d);
+	printf("%f\n", mu);
+	return 0;
 }
 
 /**
- * \brief Compute Robust Soliton Distribution for a node-degree d
- */
-double getmu(double R, int d){
+* Get Ideal Soliton distribution
+* @param d node degree
+* @return Robust Soliton probabilit
+*/
+double getro(int d){
+	double ro;
 
-  double ro;
-  double tau;
-  double beta = 0;
-  double mu;
-  /* Compute ro */
-  ro = getro(d);
-  /* Compute tau */
-  tau = gettau(R,d);
-  /* Compute beta */
-  for(int i=1;i<=K;i++){
-      beta += getro(i) + gettau(R,i);
-  }
-  /* Compute mu */
-  mu = (ro+tau)/beta;
-  return mu;
+	if(d > 1) {
+		ro = 1./(d*(d-1));
+	}
+	else {
+		ro = 1./K;
+	}
+	return ro;
+}
+
+/**
+* Tau weighting function for Robust Soliton
+* @param R Robust soliton parameter
+* @param d node degree
+* @return Robust Soliton probability for d
+*/
+double gettau(double R, int d){
+	double tau;
+
+	if(d>=1 && d<K/R) {
+		tau = R/(d*K);
+	}
+	else if(d == K/R) {
+		tau = R*log(R/delta)/K;
+	}
+	else {
+		tau = 0;
+	}
+	return tau;
+}
+
+/**
+* Compute Robust Soliton Distribution for a node-degree d
+* @param R Robust soliton parameter
+* @param d node degree
+* @return Robust Soliton probability for d
+*/
+double getmu(double R, int d){
+	double ro;
+	double tau;
+	double beta = 0;
+	double mu;
+
+	// compute ro
+	ro = getro(d);
+
+	// compute tau
+	tau = gettau(R, d);
+
+	// compute beta
+	for(int i=1; i<=K; i++) {
+		beta += getro(i) + gettau(R, i);
+	}
+
+	// compute mu
+	mu = (ro+tau)/beta;
+	return mu;
 }
